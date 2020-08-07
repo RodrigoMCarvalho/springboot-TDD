d50781cb8c31769ddd614b6613d1e71902be598c
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-08-07 20:26:00 -0300

    preparando o banco em memoria

--- src/main/java/com/rodrigo/springtdd/repository/PessoaRepository.java
+++ src/main/java/com/rodrigo/springtdd/repository/PessoaRepository.java
@@ -1,12 +1,12 @@
 package com.rodrigo.springtdd.repository;
 
 import com.rodrigo.springtdd.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PessoaRepository {
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
-    Pessoa save(Pessoa pessoa);
     Optional<Pessoa> findByCPF(String cpf);
     Optional<Pessoa> findByTelefoneDddAndTelefoneNumero(String ddd, String telefone);
     Pessoa findByTelefone(String telefone);
